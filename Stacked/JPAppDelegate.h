e290cd247648a2d48ee12aecf33343d062e688eb
pyro2927 <joseph@pintozzi.com>
2012-11-23 00:16:24 -0600

    Initial Commit

---
+++ Stacked/JPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JPAppDelegate.h
+//  Stacked
+//
+//  Created by Joseph Pintozzi on 11/23/12.
+//  Copyright (c) 2012 TinyDragon Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stacked/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stacked
+//
+//  Created by Joseph Pintozzi on 11/23/12.
+//  Copyright (c) 2012 TinyDragon Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JPAppDelegate class]));
+    }
+}
